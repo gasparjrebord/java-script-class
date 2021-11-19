cf0831d15a735bed892193ea6ec180d7c8f70f4e
Gaspar Rebord <gasparrebord31@gmail.com>
2021-11-19 14:34:50 -0300

    3th commit

--- js/index.js
+++ js/index.js
@@ -1,20 +1,31 @@
-let fullname, birthdate, age;
+let name, lastname, fullname, birthyear, age, month, birthday;
 let currentYear = 2021;
 let minAge = 18;
 
 alert("Bienvenido al primer desafio de javascript!");
 
-fullname = prompt("Introduzca su nombre completo");
+name = prompt("Cual es tu nombre de pila?");
 
-birthdate = prompt(fullname + ", en que año naciste?");
+lastname = prompt("Cual es tu apellido?");
 
-age = currentYear - birthdate;
+fullname = name + " " + lastname;
+
+birthyear = parseInt(prompt(fullname + ", en que año naciste?"));
+
+age = currentYear - birthyear;
 
 if (age > minAge){
-    alert("Felicitaciones " + fullname + ", como eres mayor de edad puedes continuar")
+    alert("Felicitaciones " + fullname + ", como tienes " + age + " años de edad puedes continuar");
+
+    birthday = prompt("Numero del dia en que naciste");
+
+    month = prompt("Nombre de tu mes de nacimiento");
+
+    alert("Naciste un " + birthday + " de " + month + " del " + birthyear + "?");
+
 };
 if (age < minAge){
-    alert("Lo siento " + fullname + ", me temo que no puedes continuar")
+    alert("Lo siento " + fullname + ", me temo que no puedes continuar por tu edad");
 };
 
 
