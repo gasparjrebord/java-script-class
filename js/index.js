d5cf1558e1565d98bfcc683b09c788a7e437cbfc
Gaspar Rebord <gasparrebord31@gmail.com>
2021-11-25 15:15:01 -0300

    ...

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 /**
- * @challenge: Desafío: crear un algoritmo con un condicional
+ * @challenge: Desafío: Crear un algoritmo utilizando un ciclo
  * 
  * @version: v1.0.0
- * @author: Indica el nombre del autor del código.
- * @fecha: 25/11/2021
+ * @author: Gaspar Rebord.
+ * @fecha: 30/11/2021
  *
  * History:
  *  - v1.0.0 – Primera entrega
@@ -14,11 +14,14 @@ let firstName, lastName, fullName, birthYear, age, month, birthDay;
 let currentYear = 2021;
 let minAge = 18;
 
-alert("Bienvenido al primer desafio de javascript!");
+alert("Bienvenido al tercer desafio de javascript!");
+alert("Para continuar primero debes registrarte")
 
 firstName = prompt("Cual es tu nombre de pila?");
 lastName = prompt("Cual es tu apellido?");
 fullName = firstName + " " + lastName;
+
+console.log("Full Name: "+ fullName);
 
 if (firstName != "" && lastName != ""){
     birthYear = parseInt(prompt(fullName + ", en que año naciste?"));
@@ -26,12 +29,10 @@ if (firstName != "" && lastName != ""){
     if (age > minAge){
 
         alert("Felicitaciones " + fullName + ", como tienes " + age + " años de edad puedes continuar");
-
         birthDay = prompt("Numero del dia en que naciste");
-
         month = prompt("Nombre de tu mes de nacimiento");
+        console.log(birthDay + "/" + month + "/" + birthYear);
 
-        alert("Naciste un " + birthDay + " de " + month + " del " + birthYear + "?");
     }else {
         alert("Lo siento " + fullName + ", me temo que no puedes continuar por tu edad");
     };
